72bcaf156fd7114d0b20064ef70a4d6486d103d9
sjweagley <scott.weagley.894@my.csun.edu>
2017-02-25 21:37:22 -0800

    Project Upload

---
+++ src/server/Server.java
@@ -0,0 +1,108 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Scott Weagley
+ * This is a code example provide from the java website 
+ * https://docs.oracle.com/javase/tutorial/networking/sockets/clientServer.html
+ * Project 3
+ */
+public class Server {
+
+       public static void main(String[] args) throws IOException {
+         
+        try ( 
+            ServerSocket serverSocket = new ServerSocket(1234);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+         
+            String inputLine, outputLine;
+             
+            // Initiate conversation with client
+            Protocol clientProtocol = new Protocol();
+            outputLine = clientProtocol.processInput(null);
+            out.println(outputLine);
+ 
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = clientProtocol.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + "1234" + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }    
+    
+    static public class Protocol {
+    private static final int WAITING = 0;
+    private static final int SENTKNOCKKNOCK = 1;
+    private static final int SENTCLUE = 2;
+    private static final int ANOTHER = 3;
+ 
+    private static final int NUMJOKES = 5;
+ 
+    private int state = WAITING;
+    private int currentJoke = 0;
+ 
+    private String[] clues = { "Turnip", "Little Old Lady", "Atch", "Who", "Who" };
+    private String[] answers = { "Turnip the heat, it's cold in here!",
+                                 "I didn't know you could yodel!",
+                                 "Bless you!",
+                                 "Is there an owl in here?",
+                                 "Is there an echo in here?" };
+ 
+    public String processInput(String theInput) {
+        String theOutput = null;
+ 
+        if (state == WAITING) {
+            theOutput = "Knock! Knock!";
+            state = SENTKNOCKKNOCK;
+        } else if (state == SENTKNOCKKNOCK) {
+            if (theInput.equalsIgnoreCase("Who's there?")) {
+                theOutput = clues[currentJoke];
+                state = SENTCLUE;
+            } else {
+                theOutput = "You're supposed to say \"Who's there?\"! " +
+                "Try again. Knock! Knock!";
+            }
+        } else if (state == SENTCLUE) {
+            if (theInput.equalsIgnoreCase(clues[currentJoke] + " who?")) {
+                theOutput = answers[currentJoke] + " Want another? (y/n)";
+                state = ANOTHER;
+            } else {
+                theOutput = "You're supposed to say \"" + 
+                clues[currentJoke] + 
+                " who?\"" + 
+                "! Try again. Knock! Knock!";
+                state = SENTKNOCKKNOCK;
+            }
+        } else if (state == ANOTHER) {
+            if (theInput.equalsIgnoreCase("y")) {
+                theOutput = "Knock! Knock!";
+                if (currentJoke == (NUMJOKES - 1))
+                    currentJoke = 0;
+                else
+                    currentJoke++;
+                state = SENTKNOCKKNOCK;
+            } else {
+                theOutput = "Bye.";
+                state = WAITING;
+            }
+        }
+        return theOutput;
+    }
+}    
+}
